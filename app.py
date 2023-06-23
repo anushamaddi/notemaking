2514564658b68cd9986776d7160da160518414cc
anushamaddi824@gmail.com <anusha.maddi@otsi.com>
2023-06-23 17:17:22 +0530

    session update"

--- app.py
+++ app.py
@@ -106,43 +106,49 @@ def register():
          return render_template("registration.html")
 @app.route('/addnote', methods =["GET", "POST"])
 def addnote():
-    if request.method == "POST":
-        # getting input with name = fname in HTML form
-        name = request.form["name"]
-        # print(name)
-        # getting input with name = lname in HTML form
-        note = request.form["note"]
-        # print(note)
+    if session.get("email"):
+        if request.method == "POST":
+            # getting input with name = fname in HTML form
+            name = request.form["name"]
+            # print(name)
+            # getting input with name = lname in HTML form
+            note = request.form["note"]
+            # print(note)
 
-        with sqlite3.connect("database.db", check_same_thread=False) as users:
-            cursor = users.cursor()
-            cursor.execute("INSERT INTO NOTES \
-            (name,note) VALUES (?,?)",
-                        (name, note))
-            users.commit()
-            print("Note created successfully")
-        return render_template("index.html")
+            with sqlite3.connect("database.db", check_same_thread=False) as users:
+                cursor = users.cursor()
+                cursor.execute("INSERT INTO NOTES \
+                (name,note) VALUES (?,?)",
+                            (name, note))
+                users.commit()
+                print("Note created successfully")
+            return render_template("index.html")
+        else:
+            return render_template('join.html')
     else:
-        return render_template('join.html')
+        return render_template('login.html') 
 @app.route('/opennotes')
 def opennotes():
-    connect = sqlite3.connect('database.db', check_same_thread=False)
-    cur = connect.cursor()
-    print("Cursor created")
-    statement='''SELECT * FROM NOTES'''
-    cur.execute(statement)
-    # # print("Cursor created",cursor)
-    # data = cursor.fetchall()
-    # print("data extracted",data)
-    
-    output = cur.fetchall()
-    for row in output:
-        print(row)
-    connect.commit()
-    
-    # Close the connection
-    connect.close()
-    return render_template("checknotes.html", data=output)
+    if session.get("email"):
+        connect = sqlite3.connect('database.db', check_same_thread=False)
+        cur = connect.cursor()
+        print("Cursor created")
+        statement='''SELECT * FROM NOTES'''
+        cur.execute(statement)
+        # # print("Cursor created",cursor)
+        # data = cursor.fetchall()
+        # print("data extracted",data)
+        
+        output = cur.fetchall()
+        for row in output:
+            print(row)
+        connect.commit()
+        
+        # Close the connection
+        connect.close()
+        return render_template("checknotes.html", data=output)
+    else:
+         return render_template("login.html")
     
 if __name__ == '__main__':
 	app.run(debug=False)
